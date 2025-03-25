6dd22d3ec90aaa88a38cea974b693677598510b6
chunjie <wcj@ebj.sx.cn>
2025-03-25 15:32:17 +0800

    根据官方文档修改配置

--- src/main/java/app/rijiben/blog/common/config/WebConfig.java
+++ src/main/java/app/rijiben/blog/common/config/WebConfig.java
@@ -17,19 +17,22 @@ import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.format.datetime.DateFormatter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.DelegatingWebMvcConfiguration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 import javax.sql.DataSource;
 
-@Configuration
-@EnableWebMvc
+
 @EnableAspectJAutoProxy
 @ComponentScan(basePackageClasses = BlogStart.class)
 @MapperScan(basePackages = Constr.MAPPER_SCAN)
 @EnableTransactionManagement
-public class WebConfig implements WebMvcConfigurer {
+@Configuration
+public class WebConfig extends DelegatingWebMvcConfiguration {
     private final Logger log = LoggerFactory.getLogger(getClass());
 
     @Bean("dataSource")
@@ -58,9 +61,9 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
     @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(authConfig()).addPathPatterns(Constr.ADM_URL.concat("**")).excludePathPatterns(new String[]{Constr.ADM_URL,Constr.ADM_URL.concat("login"),Constr.ADM_URL.concat("captcha")});
-        WebMvcConfigurer.super.addInterceptors(registry);
+    protected void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authConfig()).addPathPatterns(Constr.ADM_URL.concat("**")).excludePathPatterns(Constr.ADM_URL, Constr.ADM_URL.concat("login"), Constr.ADM_URL.concat("captcha"));
+        super.addInterceptors(registry);
     }
 
     @Override
@@ -71,19 +74,19 @@ public class WebConfig implements WebMvcConfigurer {
         resolver.setRequestContextAttribute("req");
         resolver.setViewClass(JstlView.class);
         registry.viewResolver(resolver);
-        WebMvcConfigurer.super.configureViewResolvers(registry);
+        super.configureViewResolvers(registry);
     }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler(Constr.RESOURCE_PATH.concat("**")).addResourceLocations(Constr.RESOURCE_PATH);
-        WebMvcConfigurer.super.addResourceHandlers(registry);
+        super.addResourceHandlers(registry);
     }
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addFormatter(new DateFormatter(Constr.DEFAULT_FMT));
-        WebMvcConfigurer.super.addFormatters(registry);
+        super.addFormatters(registry);
     }
 
 }
